49a362ac0a23a5fe8e7aae0cd7ac11fe3d96e29a
Stuart Summers <summers2483@hotmail.com>
2021-03-31 11:53:40 +0200

    First commit with caching and wildcard constraining examples

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Blog</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My first post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aspernatur assumenda aut consectetur cupiditate dicta dolore ea et labore maiores, molestias nobis perspiciatis provident quaerat qui ratione sapiente voluptas voluptatum. Beatae consectetur, deleniti doloremque exercitationem expedita explicabo ipsam ipsum labore molestias nesciunt perspiciatis praesentium quam repudiandae, rerum, ullam? Laborum, temporibus!</p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-second-post">My second post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aspernatur assumenda aut consectetur cupiditate dicta dolore ea et labore maiores, molestias nobis perspiciatis provident quaerat qui ratione sapiente voluptas voluptatum. Beatae consectetur, deleniti doloremque exercitationem expedita explicabo ipsam ipsum labore molestias nesciunt perspiciatis praesentium quam repudiandae, rerum, ullam? Laborum, temporibus!</p>
+</article>
+
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('posts/{post}', function ($slug){
+
+        $path = __DIR__ . "/../resources/posts/{$slug}.html";
+        if (!file_exists($path))
+        {
+            return redirect('/');
+        };
+
+        //basic caching example
+        $post = cache()->remember('posts.{slug}', now()->addSeconds(5),
+            function () use ($path)
+            {
+                var_dump('file_get_contents');
+                return  file_get_contents($path);
+            });
+
+        return view('post', [
+            'post' => $post
+        ]);
+    })->where('post', '[a-z_\-]+'); //Wildcard constraint
+
